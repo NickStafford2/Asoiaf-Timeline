31d1d83972fa3fa215186f31506832b5683cdb98
fuckMS <microsoft@sucks.com>
2023-04-25 23:20:15 -0400

    esling config. not perfect yet

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,80 @@
 module.exports = {
-  "root": true,
-  "ignorePatterns": [
-    "projects/**/*"
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
   ],
-  "overrides": [
-    {
-      "files": [
-        "*.ts"
-      ],
-      "parserOptions": {
-        "project": [
-          "tsconfig.json"
-        ],
-        "tsconfigRootDir": __dirname,
-        "createDefaultProgram": true
-      },
-      "extends": [
-        "eslint:recommended",
-        //"plugin:@typescript-eslint/reccomended"
-        "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates"
-      ],
-      "rules": {
-        "@angular-eslint/directive-selector": [
-          "error",
-          {
-            "type": "attribute",
-            "prefix": "app",
-            "style": "camelCase"
-          }
-        ],
-        "@angular-eslint/component-selector": [
-          "error",
-          {
-            "type": "element",
-            "prefix": "app",
-            "style": "kebab-case"
-          }
-        ],
-        //"@typescript-eslint/array-type": "warn",
-        //"member-access": [true, "no-public"],
-
-
-      }
-    },
-    {
-      "files": [
-        "*.html"
-      ],
-      "extends": [
-        "plugin:@angular-eslint/template/recommended"
-      ],
-      "rules": {}
-    }
-  ]
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: [
+      'tsconfig.json'
+    ],
+    tsconfigRootDir: __dirname,
+    createDefaultProgram: true
+  },
+  rules: {
+  }
 }
+
+/*
+module.exports = {
+  "root": true,
+  "ignorePatterns": [ */
+//  "projects/**/* "
+/*  ],
+"overrides": [
+  {
+    "files": [
+      "*.ts"
+    ],
+    "parserOptions": {
+      "project": [
+        "tsconfig.json"
+      ],
+      "tsconfigRootDir": __dirname,
+      "createDefaultProgram": true
+    },
+    "extends": [
+      "eslint:recommended",
+      //"plugin:@typescript-eslint/reccomended"
+      "plugin:@angular-eslint/recommended",
+      "plugin:@angular-eslint/template/process-inline-templates"
+    ],
+    "rules": {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          "type": "attribute",
+          "prefix": "app",
+          "style": "camelCase"
+        }
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          "type": "element",
+          "prefix": "app",
+          "style": "kebab-case"
+        }
+      ],
+      //"@typescript-eslint/array-type": "warn",
+      //"member-access": [true, "no-public"],
+
+    }
+  },
+  {
+    "files": [
+      "*.html"
+    ],
+    "extends": [
+      "plugin:@angular-eslint/template/recommended"
+    ],
+    "rules": {}
+  }
+]
+}
+
+*/
