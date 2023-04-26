7cfbfab58e2cb63b0c9d60c54ac4bb2b4e451c18
fuckMS <microsoft@sucks.com>
2023-04-26 00:32:50 -0400

    eslint working with prettier and angular-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,69 @@
+// https://gist.github.com/eneajaho/17bbcf71c44eabf56d404b028572b97b
+// https://github.com/angular-eslint/angular-eslint#notes-for-eslint-plugin-prettier-users
 module.exports = {
+  "root": true,
+    "ignorePatterns": ["projects/**/*"],
+      "overrides": [
+        {
+          "files": ["*.ts"],
+          "parserOptions": {
+            "project": ["tsconfig.json"],
+            tsconfigRootDir: __dirname,
+            "createDefaultProgram": true
+          },
+          "extends": [
+            "plugin:@angular-eslint/recommended",
+            "plugin:@angular-eslint/template/process-inline-templates",
+            "plugin:prettier/recommended"
+          ],
+          "rules": {
+            "@angular-eslint/component-class-suffix": [
+              "error",
+              {
+                "suffixes": ["Page", "Component"]
+              }
+            ],
+            "@angular-eslint/component-selector": [
+              "error",
+              {
+                "type": "element",
+                "prefix": "app",
+                "style": "kebab-case"
+              }
+            ],
+            "@angular-eslint/directive-selector": [
+              "error",
+              {
+                "type": "attribute",
+                "prefix": "app",
+                "style": "camelCase"
+              }
+            ],
+            "@typescript-eslint/member-ordering": 0,
+            "@typescript-eslint/naming-convention": 0
+          }
+        },
+        // NOTE: WE ARE NOT APPLYING PRETTIER IN THIS OVERRIDE, ONLY @ANGULAR-ESLINT/TEMPLATE
+        {
+          "files": ["*.html"],
+          "extends": ["plugin:@angular-eslint/template/recommended"],
+          "rules": {}
+        },
+        // NOTE: WE ARE NOT APPLYING @ANGULAR-ESLINT/TEMPLATE IN THIS OVERRIDE, ONLY PRETTIER
+        {
+          "files": ["*.html"],
+          "excludedFiles": ["*inline-template-*.component.html"],
+          "extends": ["plugin:prettier/recommended"],
+          "rules": {
+            // NOTE: WE ARE OVERRIDING THE DEFAULT CONFIG TO ALWAYS SET THE PARSER TO ANGULAR (SEE BELOW)
+            "prettier/prettier": ["error", { "parser": "angular" }]
+          }
+        }
+      ]
+}
+
+/*
+const try2 = {
   env: {
     browser: true,
     es2021: true
@@ -18,13 +83,13 @@ module.exports = {
   rules: {
   }
 }
-
-
-const x = {
+*/
+/*
+const try1 = {
   "root": true,
-  "ignorePatterns": [
-    "projects/**/* "
-  ],
+  "ignorePatterns": [*/
+  //  "projects/**/* " 
+/*  ],
   "overrides": [
     {
       "files": [
@@ -75,3 +140,4 @@ const x = {
     }
   ]
 }
+*/
