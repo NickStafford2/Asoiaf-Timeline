dfdb466c58a3cb96e12ea85c0a9a09dc8358f117
fuckMS <microsoft@sucks.com>
2023-04-26 01:25:24 -0400

    eslintrc style update. removed quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,66 +1,70 @@
+// ?todo: this one might be better: https://github.com/bzvyagintsev/angular-boilerplate/blob/master/.eslintrc.js
+// https://dev.to/bzvyagintsev/migrate-angular-app-to-eslint-with-prettier-airbnb-styleguide-husky-and-lint-staged-862
+
+// current
 // https://gist.github.com/eneajaho/17bbcf71c44eabf56d404b028572b97b
 // https://github.com/angular-eslint/angular-eslint#notes-for-eslint-plugin-prettier-users
 module.exports = {
-  "root": true,
-    "ignorePatterns": ["projects/**/*"],
-      "overrides": [
-        {
-          "files": ["*.ts"],
-          "parserOptions": {
-            "project": ["tsconfig.json"],
-            tsconfigRootDir: __dirname,
-            "createDefaultProgram": true
+  root: true,
+  ignorePatterns: ['projects/**/*'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: ['tsconfig.json'],
+        tsconfigRootDir: __dirname,
+        createDefaultProgram: true,
+      },
+      extends: [
+        'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        '@angular-eslint/component-class-suffix': [
+          'error',
+          {
+            suffixes: ['Page', 'Component'],
           },
-          "extends": [
-            "plugin:@angular-eslint/recommended",
-            "plugin:@angular-eslint/template/process-inline-templates",
-            "plugin:prettier/recommended"
-          ],
-          "rules": {
-            "@angular-eslint/component-class-suffix": [
-              "error",
-              {
-                "suffixes": ["Page", "Component"]
-              }
-            ],
-            "@angular-eslint/component-selector": [
-              "error",
-              {
-                "type": "element",
-                "prefix": "app",
-                "style": "kebab-case"
-              }
-            ],
-            "@angular-eslint/directive-selector": [
-              "error",
-              {
-                "type": "attribute",
-                "prefix": "app",
-                "style": "camelCase"
-              }
-            ],
-            "@typescript-eslint/member-ordering": 0,
-            "@typescript-eslint/naming-convention": 0
-          }
-        },
-        // NOTE: WE ARE NOT APPLYING PRETTIER IN THIS OVERRIDE, ONLY @ANGULAR-ESLINT/TEMPLATE
-        {
-          "files": ["*.html"],
-          "extends": ["plugin:@angular-eslint/template/recommended"],
-          "rules": {}
-        },
-        // NOTE: WE ARE NOT APPLYING @ANGULAR-ESLINT/TEMPLATE IN THIS OVERRIDE, ONLY PRETTIER
-        {
-          "files": ["*.html"],
-          "excludedFiles": ["*inline-template-*.component.html"],
-          "extends": ["plugin:prettier/recommended"],
-          "rules": {
-            // NOTE: WE ARE OVERRIDING THE DEFAULT CONFIG TO ALWAYS SET THE PARSER TO ANGULAR (SEE BELOW)
-            "prettier/prettier": ["error", { "parser": "angular" }]
-          }
-        }
-      ]
-}
+        ],
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case',
+          },
+        ],
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase',
+          },
+        ],
+        '@typescript-eslint/member-ordering': 0,
+        '@typescript-eslint/naming-convention': 0,
+      },
+    },
+    // NOTE: WE ARE NOT APPLYING PRETTIER IN THIS OVERRIDE, ONLY @ANGULAR-ESLINT/TEMPLATE
+    {
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {},
+    },
+    // NOTE: WE ARE NOT APPLYING @ANGULAR-ESLINT/TEMPLATE IN THIS OVERRIDE, ONLY PRETTIER
+    {
+      files: ['*.html'],
+      excludedFiles: ['*inline-template-*.component.html'],
+      extends: ['plugin:prettier/recommended'],
+      rules: {
+        // NOTE: WE ARE OVERRIDING THE DEFAULT CONFIG TO ALWAYS SET THE PARSER TO ANGULAR (SEE BELOW)
+        'prettier/prettier': ['error', { parser: 'angular' }],
+      },
+    },
+  ],
+};
 
 /*
 const try2 = {
