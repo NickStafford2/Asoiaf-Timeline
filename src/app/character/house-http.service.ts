f285cec86f4bcc21ee74d78b3801a9e1d826a7a6
fuckMS <microsoft@sucks.com>
2023-04-26 21:08:57 -0400

    before adding eslint sort imports

--- src/app/character/house-http.service.ts
+++ src/app/character/house-http.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { ConfigService } from '../config.service';
+import { House, HouseData } from './house.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HouseHttpService {
+  private static readonly URL: string = 'api/Houses';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<House[]> {
+    return this.http.get<House[]>(HouseHttpService.URL).pipe(
+      tap(() => console.log('getAll')),
+      catchError(ConfigService.handleError<House[]>('getAll', []))
+    );
+  }
+
+  create(newHouse: House) {
+    return this.http.post<House>(HouseHttpService.URL, newHouse).pipe(
+      tap(() => console.log('create')),
+      catchError(ConfigService.handleError<House[]>('create', []))
+    );
+  }
+
+  update(house: House) {
+    return this.http
+      .put<HouseData>(HouseHttpService.URL + '/' + house.id, house)
+      .pipe(
+        tap(() => console.log('update')),
+        catchError(ConfigService.handleError<HouseData[]>('update', []))
+      );
+  }
+
+  delete(houseId: string) {
+    return this.http
+      .delete<HouseData>(HouseHttpService.URL + '/' + houseId)
+      .pipe(
+        tap(() => console.log('delete')),
+        catchError(ConfigService.handleError<HouseData[]>('delete', []))
+      );
+  }
 }
--- src/app/character/house.service.ts
+++ src/app/character/house.service.ts
@@ -1,56 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { ConfigService } from '../config.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HouseService {
-  private static readonly URL: string = 'api/Houses';
-
-  constructor(private http: HttpClient) { }
-
-  getAll(): Observable<CharacterData[]> {
-    return this.http.get<CharacterData[]>(HouseService.URL).pipe(
-      tap(() => console.log('getAll')),
-      catchError(ConfigService.handleError<CharacterData[]>('getAll', []))
-    );
-  }
-
-  create(newHouse: House) {
-    return this.http
-      .post<House>(HouseService.URL, newHouse)
-      .pipe(
-        tap(() => console.log('create')),
-        catchError(ConfigService.handleError<House[]>('create', []))
-      );
-  }
-
-  update(updatedCharacter: CharacterData) {
-    const c = new CharacterClass(updatedCharacter);
-    const cbody2 = c.toObject();
-    return this.http
-      .put<CharacterData>(HouseService.URL + '/' + cbody2.id, cbody2)
-      .pipe(
-        tap(() => console.log('update')),
-        catchError(ConfigService.handleError<CharacterData[]>('update', []))
-      );
-  }
-
-  delete(houseId: string) {
-    return this.http
-      .delete<CharacterData>(HouseService.URL + '/' + houseId)
-      .pipe(
-        tap(() => console.log('delete')),
-        catchError(ConfigService.handleError<CharacterData[]>('delete', []))
-      );
-  }
+  constructor(private http: HttpClient) {}
 }
 
-export interface House {
+export interface House extends HouseData {
   id: string;
+}
+
+export interface HouseData {
   name: string;
   type: HouseType;
 }
